f5c1520fcba306b00d1e599d749ce16666e3e2f9
Freezie75 <nathanbittoun@yahoo.fr>
2018-04-24 23:02:36 +0200

    Terminer à moitier

--- The Hacking Class/db/schema.rb
+++ The Hacking Class/db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_04_24_204845) do
+
+  create_table "cours", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "eleves", force: :cascade do |t|
+    t.string "name"
+    t.string "matiere"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
